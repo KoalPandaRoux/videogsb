456da32721f52de93f6abec5dbba0b85d993e1ec
Louis Chesnais-Bordes <louischesnaisbordes@gmail.com>
2016-03-06 21:42:00 +0100

    Update 404.php

--- error/404.php
+++ error/404.php
@@ -1,14 +1,13 @@
-
-  <h2>Oups !</h2>
-  <p>Vous n'avez pas accès à cette section.</p>
-  <p>Vous allez être redirigé dans quelques secondes...</p>
+<h2>Oups !</h2>
+<p>Vous n'avez pas accès à cette section.</p>
+<p>Vous allez être redirigé dans quelques secondes...</p>
 
 <?php
 
+//Le temps de connexion avant redirection (en seconde)
 $timeConnect = 3;
 $urlConnect = 'url=http://localhost/videogsb/index.php';
 
-//error
 header("Refresh:$timeConnect; $urlConnect");
 
 
